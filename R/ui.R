fbb7cdbde68814541942eabd67fc73b004016416
Mario Dejung <mario@dejung.net>
2016-06-15 11:33:51 +0200

    moved ui and server in extra files

---
+++ R/ui.R
@@ -0,0 +1,21 @@
+ui <- shiny::shinyUI(shiny::fluidPage(
+
+  # Application title
+  shiny::titlePanel("Old Faithful Geyser Data"),
+
+  # Sidebar with a slider input for number of bins
+  shiny::sidebarLayout(
+    shiny::sidebarPanel(
+      shiny::sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    shiny::mainPanel(
+      shiny::plotOutput("distPlot")
+    )
+  )
+))
